5749e5ea8822e837a0cefa55d0a0bad8c782873b
harryconod <118738331+harryconod@users.noreply.github.com>
2025-03-02 18:14:25 +0000

    Added methods to ticket class for exercise 02_04

--- _02_04/Ticket.java
+++ _02_04/Ticket.java
@@ -12,8 +12,29 @@ public class Ticket {
 
   // Add three methods to set the value of each field, called setDestination,
   // setPrice and setIsReturn.
+  public void setPrice(double price) {
+    this.price = price;
+  }
+
+  public void setDestination(String destination) {
+    this.destination = destination;
+  }
+
+  public void setIsReturn(boolean isReturn) {
+    this.isReturn = isReturn;
+  }
 
   // Add a separate method to get the value of each field, called getDestination,
   // getPrice and getIsReturn.
+  public double getPrice() {
+    return price;
+  }
 
+  public String getDestination() {
+    return destination;
+  }
+
+  public boolean getIsReturn() {
+    return isReturn;
+  }
 }
